ad3f6b007362bb304b1406721234fc5912e77902
Shulyak Nikolay <shulyak.nikolay@list.ru>
2025-10-29 16:49:04 +0700

    Разработана практическа работа номер 6

--- console/printAccumulator.c
+++ console/printAccumulator.c
@@ -1,26 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <myBigChars.h>
 #include <mySimpleComputer.h>
 #include <myTerm.h>
 
 void
-printAccumulator (void)
+printCell (int address, enum colors fg, enum colors bg)
 {
-  bc_box (1, 62, 21, 1, DEFAULT, DEFAULT, " Аккумулятор ", RED, BLACK);
-  int value;
-  mt_gotoXY (2, 63);
+  int value = 0;
+  mt_setfgcolor (fg);
+  mt_setbgcolor (bg);
+  mt_gotoXY (address / 10 + 2, 6 * (address % 10) + 2);
+  sc_memoryGet (address, &value);
 
-  mt_print (" sc: ");
-  sc_accumulatorGet (&value);
+  int command = (value >> 7) & 0b1111111;
+  int operand = value & 0b1111111;
+
   if (value >> 14)
     mt_print ("-");
   else
     mt_print ("+");
 
-  int command = (value >> 7) & 0b1111111;
-  int operand = value & 0b1111111;
-  mt_print ("%02X%02X ", command, operand);
-  mt_print ("hex: %04X", value);
+  mt_print ("%02X%02X", command, operand);
+  mt_setdefaultcolor ();
 }
